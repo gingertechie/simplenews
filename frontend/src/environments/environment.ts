025dc06cbe7a0b892cbc001c9b8dfc1bced2ac19
Dave Anderson <dave@gingertechie.com>
2020-06-06 19:45:29 +0100

    hardcoding URLs is fun

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiURL: 'http:localhost:1337',
+  production: true,
+  apiURL: 'https:simplenews-backend.herokuapp.com',
 };
 
 /*
